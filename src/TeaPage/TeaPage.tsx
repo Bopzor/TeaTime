d650638ea57c99ab5f8b6616cd7afd4364f55cf6
Bopzor <violaine.ballandin@gmail.com>
2019-04-16 17:12:51 +0200

    store current Tea in localStorage. In that way, can display the tea after page reload

--- src/TeaPage/TeaPage.tsx
+++ src/TeaPage/TeaPage.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect, useState } from 'react';
 import moment from 'moment';
 
 import {
@@ -17,18 +17,36 @@ type TeaPageProps = {
 };
 
 export const TeaPage: FunctionComponent<TeaPageProps> = ({ tea }) => {
-  const { name, brand, temperature, time, id } = tea;
+  const [currentTea, setCurrentTea] = useState<Tea | null>(tea);
+
+  useEffect(() => {
+    if (!currentTea) {
+      const fetchedTea = localStorage.getItem('current-tea');
+      setCurrentTea(parsedTea(fetchedTea));
+    } else {
+      localStorage.setItem('current-tea', JSON.stringify(tea));
+    }
+  }, [currentTea]);
+
+  const parsedTea = (fetchedTea: string | null) => {
+    if (!fetchedTea)
+      return null;
+    return JSON.parse(fetchedTea);
+  };
+
+  if (!currentTea)
+    return null;
 
   return (
     <div style={teaPageWrapper}>
       <div style={teaPageNameWrapper}>
-        <h2 style={teaPageName}>{ name }</h2>
-        <div style={teaPageBrand}>{ brand }</div>
+        <h2 style={teaPageName}>{ currentTea.name }</h2>
+        <div style={teaPageBrand}>{ currentTea.brand }</div>
       </div>
 
-      <div style={teaPageTemperature}>{ temperature }°C</div>
+      <div style={teaPageTemperature}>{ currentTea.temperature }°C</div>
 
-      <Timer time={moment.duration(time)} />
+      <Timer time={moment.duration(currentTea.time)} />
 
     </div>
 
