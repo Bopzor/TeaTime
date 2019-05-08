548bda06889a477221320921edb99599688a742f
Bopzor <violaine.ballandin@gmail.com>
2019-05-08 16:54:42 +0200

    fix setInterval type

--- src/worker.ts
+++ src/worker.ts
@@ -11,7 +11,7 @@ const worker = () => {
   let timerId: number | undefined = undefined;
 
   const startTimer = (): number => {
-    return window.setInterval(() => {
+    return <any>setInterval(() => {
       if(duration > 0) {
           --duration;
           ctxWorker.postMessage({ type: 'DURATION', duration });
